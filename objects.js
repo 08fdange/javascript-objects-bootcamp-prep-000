a7ecd0e12450348b80bcf95fd18ecf9058ba5038
Frank  D'Angelo <08fdange@gmail.com>
2020-01-22 21:03:03 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,15 +1,1 @@
-function updateObjectWithKeyAndValue(obj, key, value) {
-  return Object.assign({}, obj, {[key]: value})
-}
-
-const recipe = {eggs: 3}
-
-console.log(updateObjectWithKeyAndValue(recipe, 'chocolate', '1 cup'))
-
-console.log(recipe)
-
-function updateObjectWithObject(targetObject, updatesObject) {
-  return Object.assign({}, targetObject, updatesObject)
-}
-
 
